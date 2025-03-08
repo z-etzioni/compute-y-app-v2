23e033fee298628a5b105a2a4e9ca4194a618419
z-etzioni <zachary.etzioni@emory.edu>
2025-03-08 15:47:18 +0000

    app1.py

--- app1.py
+++ app1.py
@@ -18,6 +18,8 @@ st.text("2. The total number of publications across all journals")
 st.text("3. The total number of publications in the top 5 Economics journals")
 st.text("4. Status as an Associate or Full Professor")
 
+st.text("Input each of these values below to determine your projected salary!")
+
 st.markdown("""
     <style>
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap');
